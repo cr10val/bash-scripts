21a243bba35743ee6a03ca4ab89b2393aa48e949
Cristóbal O. Valiente <cr1val@pm.me>
2021-11-17 19:07:19 +0000

    Change the comments to english

--- public-ip.sh
+++ public-ip.sh
@@ -1,6 +1,9 @@
 
 #!/bin/bash
 ###################################################
+# This bash script get the public ip and send and 
+# telegram message when it detect a change.
+# 
 # TODO: CHECK if the string we get it's a valid IP
 ###################################################
 EXT_IP_FILE="/etc/ip_publica"
@@ -19,16 +22,15 @@ LOG_LEVEL=1
 
 if [ $LOG_LEVEL -ge 2 ]; then echo $curDate $timestamp "--- Comprobando IP Publica ---" >> $LOG_FILE; fi
 
-#Primero comprobamos que haya internet
+#Check if are internet conection
 if ping -c1 google.com &>/dev/null; then
 
         if [ $LOG_LEVEL -ge 2 ]; then echo $curDate $timestamp "*** HAY INTERNET" >> $LOG_FILE; fi
 
-        #Conseguir la IP pública
+        #Get Public IP
         CURRENT_IP=`wget -q -O - ifconfig.me/ip`
 
-        #Si falla al conseguir la IP volvemos a buscar la ip en ipinfo.io
-        # Parece que si no lo ponemos entre comillas no comprueba bien
+        #If there is an error we try to get it again from ipinfo.io                
         if [ "$CURRENT_IP" = "" ]; then
                 if [ $LOG_LEVEL -ge 1 ]; then echo $curDate $timestamp "*** ERROR IP ifconfig <<"$CURRENT_IP">>" >> $LOG_FILE; fi
                 CURRENT_IP=`curl ipinfo.io/ip`
@@ -36,37 +38,37 @@ if ping -c1 google.com &>/dev/null; then
                 if [ $LOG_LEVEL -ge 2 ]; then `telegram-send -g   $curDate" "$timestamp" ERROR ipinfo.io IP:<"$CURRENT_IP">" &`; fi
         fi
 
-        # Si la ip obtenida no esta vacia, tenemos IP
+        # If there is not an empty ip
         if [ "$CURRENT_IP" != "" ]; then
 
-                #Si existe el archivo se saca la IP que había
+                #We check for the ip file and if it's exist we get the known ip
                 if [ -f $EXT_IP_FILE ]; then
                         KNOWN_IP=$(cat $EXT_IP_FILE)
                 else
-                        #Si no existe el archivo se crea y se guarda la IP pública actual
+                        #If the file does not exist we create it and save the actual ip
                         KNOWN_IP= touch $EXT_IP_FILE
                         if [ $LOG_LEVEL -ge 2 ]; then echo $curDate $timestamp "*** ARCHIVO DE CONFIGURACION NO ENCONTRADO " >> $LOG_FILE; fi
                 fi
 
                 if [ $LOG_LEVEL -ge 3 ]; then echo $curDate $timestamp "*** KNOWN IP: <<"$KNOWN_IP">>" >> $LOG_FILE; fi
 
-                #Comprueba si la IP actual ha cambiado de la IP que se había guardado
+                #Check if the actual ip if different from then known ip
                 if [ "$CURRENT_IP" != "$KNOWN_IP" ]; then
                         echo $CURRENT_IP > $EXT_IP_FILE
-                        #Si es distinta la IP se envía un TG con la IP actual
+                       #If it detect a change we send a tg message
                         telegram-send -g  "NUEVA IP: "$CURRENT_IP &
-                        #Se guardan las IPs en un archivo de log
+                        #Log the new ip
                         echo $curDate $timestamp "* NUEVA IP: " $CURRENT_IP >> $LOG_FILE
                 else
                         if [ $LOG_LEVEL -ge 2 ]; then echo $curDate $timestamp "* Misma IP: " $CURRENT_IP >> $LOG_FILE; fi
                         #telegram-send -g  "Misma IP: "$CURRENT_IP &
                 fi
         else
-                #se escribe en el registro, error
+                #If there isn't ip we log the error
                 if [ $LOG_LEVEL -ge 1 ]; then echo $curDate $timestamp "*** ERROR IP: <<"$CURRENT_IP">>" >> $LOG_FILE; fi
         fi
 
 else
-        #Si no hay internet, lo escribimos en el registro y salimos
+        #If there isn't internet conection and exit
         if [ $LOG_LEVEL -ge 1 ]; then echo $curDate $timestamp "*** NO HAY INTERNET. Salimos" $CURRENT_IP >> $LOG_FILE; fi
 fi
